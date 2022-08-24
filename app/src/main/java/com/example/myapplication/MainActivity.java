4647a5b533de073087268a7df4f2b6ddb725f1f9
ronalkav <ronalkav@gmail.com>
2022-08-24 11:03:36 -0500

    implemented try catch blocks to notify of error

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,12 +18,18 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
+        try {
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int sum = num1 + num2;
 
-        numberSumTV.setText("" + sum);
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int sum = num1 + num2;
+
+            numberSumTV.setText("" + sum);
+        }
+        catch(Exception e){
+            numberSumTV.setText("ERROR");
+        }
     }
 
     public void findDifference(View view) {
@@ -31,11 +37,16 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberDifferenceTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int difference = num1 - num2;
+        try {
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int difference = num1 - num2;
 
-        numberDifferenceTV.setText("" + difference);
+            numberDifferenceTV.setText("" + difference);
+        }
+        catch(Exception e){
+            numberDifferenceTV.setText("ERROR");
+        }
     }
 
     public void findProduct(View view) {
@@ -43,11 +54,17 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberProductTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int product = num1 * num2;
+        try {
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int product = num1 * num2;
 
-        numberProductTV.setText("" + product);
+            numberProductTV.setText("" + product);
+        }
+        catch(Exception e){
+            numberProductTV.setText("ERROR");
+        }
+
     }
 
     public void findQuotient(View view) {
@@ -55,11 +72,17 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberQuotientTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int quotient = num1 / num2;
+        try {
 
-        numberQuotientTV.setText("" + quotient);
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int quotient = num1 / num2;
+
+            numberQuotientTV.setText("" + quotient);
+        }
+        catch(Exception e){
+            numberQuotientTV.setText("ERROR");
+        }
     }
 
 //    public void changeColor(View view){
@@ -70,10 +93,17 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.num1ET);
         TextView numberRootTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int root = (int) Math.sqrt(num1);
+        try {
 
-        numberRootTV.setText("" + root);
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int root = (int) Math.sqrt(num1);
+
+            numberRootTV.setText("" + root);
+        }
+        catch(Exception e){
+            numberRootTV.setText("ERROR");
+        }
+
     }
 
     //ideas: push out notification if they put in the wrong info, more fxns, change color of buttons, distance formula, etc., use gradient, different fonts.
